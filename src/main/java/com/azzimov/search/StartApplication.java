038b150d4eb86c37a2c3d6b0140be54370328b86
rahulpondy <rahul91ktr@yahoo.ca>
2017-11-15 13:51:44 -0500

    added sample cron task

--- src/main/java/com/azzimov/search/StartApplication.java
+++ src/main/java/com/azzimov/search/StartApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * Created by RahulGupta on 2017-11-08.
@@ -14,6 +15,7 @@ import org.springframework.context.annotation.ComponentScan;
 @EnableAutoConfiguration
 @ComponentScan
 @ServletComponentScan
+@EnableScheduling
 public class StartApplication {
     public static void main(String[] args){
         SpringApplication.run(StartApplication.class, args);
